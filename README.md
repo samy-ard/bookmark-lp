cd65893e00be9e47717a8a6b1f0dec11d1e2ae5b
samy-ard <112376794+samy-ard@users.noreply.github.com>
2023-05-09 13:20:22 +0300

    initial deploy

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Bookmark landing page solution
+
+This is a solution to the [Bookmark landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bookmark-landing-page-5d0b588a9edda32581d29158). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the newsletter form is submitted if:
+  - The input field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+![](./screenshot.png) 
+![](./screenshot-mobile.png) 
+![](./screenshot-mobile-nav.png) 
+
+### Links
+
+- Solution URL: [github](https://github.com/samy-ard/bookmark-lp/)
+- Live Site URL: [live](https://samy-ard.github.io/bookmark-lp/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@samy-ard](https://www.frontendmentor.io/profile/samy-ard)
+
+## Acknowledgments
+
+- Bootstrap (https://getbootstrap.com/docs/5.0/components/navs-tabs/)
---
+++ app.js
@@ -0,0 +1,72 @@
+(function($) {
+	function isEmail(email) {
+  	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  	return regex.test(email);
+	}
+
+	$(document).ready(function() {
+		if( $(document).width() >= 1200 ) {
+			$('.extension-col').each(function(i, val) {
+				$(val).css('margin-top', (i * 2.5) + 'rem');
+			});
+		}
+
+		$('.btn-menu').on('click', function(e) {
+			e.preventDefault();
+			var $menu = $(this).parent().find('.menu');
+			$('.modal').addClass('shown');
+			$('.modal-body').append($menu.clone());
+			$('html').addClass('modalOn');
+		});
+
+		$('.btn-close').on('click', function(e) {
+			e.preventDefault();
+			$('.modal-body').find('.menu').remove();
+			$('.modal').removeClass('shown');
+			$('html').removeClass('modalOn');
+		});
+
+		$('.nav-tab-link').on('click', function(e) {
+			e.preventDefault();
+			var $tabLink = $(this);
+			$tabLink.parent().parent().find('.nav-tab-link').removeClass('active');
+			$tabLink.parent().parent().find('.nav-tab-link').attr('aria-selected', 'false');
+			$tabLink.addClass('active');
+			$tabLink.attr('aria-selected', 'true');
+			$($tabLink.data('target')).parent().find('.tab-pane').removeClass('show').removeClass('active');
+			$($tabLink.data('target')).addClass('show').addClass('active');
+		});
+
+		$('.accordion-button').on('click', function(e) {
+			e.preventDefault();
+			var $accBtn = $(this);
+			if( $accBtn.attr('aria-expanded') === 'false' ) {
+				$accBtn.parent().parent().parent().find('.accordion-button').removeClass('active');
+				$accBtn.parent().parent().parent().find('.accordion-button').attr('aria-expanded', 'false');
+				$($accBtn.data('target')).parent().parent().find('.accordion-collapse').removeClass('show').removeClass('active');
+				$accBtn.attr('aria-expanded', 'true');
+				$accBtn.addClass('active');
+				$($accBtn.data('target')).addClass('show').addClass('active');
+			} else {
+				$accBtn.attr('aria-expanded', 'false');
+				$accBtn.removeClass('active');
+				$($accBtn.data('target')).removeClass('show').removeClass('active');
+			}
+		});
+
+		$('.contact-form').on('submit', function(e){
+			e.preventDefault();
+			var $form = $(this);
+			var $email = $form.find('input[name="email"]');
+			$form.find('.form-error').remove();
+			$email.removeClass('field-error');
+			$email.parent().removeClass('has-error');
+
+			if( isEmail($email.val()) === false ) {
+				$email.addClass('field-error');
+				$email.parent().addClass('has-error');
+				$email.parent().append("<p class=\"form-error\">Whoops, make sure it's an email</p>");
+			}
+		});
+	});
+})(jQuery);
